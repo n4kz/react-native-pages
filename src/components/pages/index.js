bed43b84fe34b18cf9b039448ea39e68bfb0060e
syzer3@gmail.com <syzer3@gmail.com>
2017-08-30 16:05:11 +0200

    Fix issue when this.scroll was not refined, ex: after requestedAnimationFrame on not existing list.

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -147,7 +147,7 @@ export default class Pages extends PureComponent {
       this.scrollState = 1;
     }
 
-    this.scroll.scrollTo({
+    this.scroll && this.scroll.scrollTo({
       [horizontal? 'x' : 'y']: page * base,
       animated,
     });
