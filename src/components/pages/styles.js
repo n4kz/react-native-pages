6a4c81a1a078a62245a9ac66b85aa40bb701253e
Suraj Tiwari <surajtiwari020@gmail.com>
2019-03-30 22:49:30 +0530

    added, topLeft, bottomLeft, topRight, bottomRight

--- src/components/pages/styles.js
+++ src/components/pages/styles.js
@@ -1,41 +1,62 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
   rtl: {
-    transform: [{
-      rotate: '180deg',
-    }],
+      transform: [{
+          rotate: '180deg',
+      }],
   },
 
   container: {
-    flex: 1,
+      flex: 1,
   },
 
   bottom: {
-    position: 'absolute',
-    right: 0,
-    bottom: 0,
-    left: 0,
+      position: 'absolute',
+      right: 0,
+      bottom: 0,
+      left: 0,
   },
 
   top: {
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    left: 0,
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      left: 0,
   },
 
   left: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
   },
 
   right: {
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    bottom: 0,
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      bottom: 0,
   },
+  topLeft: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+  },
+  bottomLeft: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+  },
+  topRight: {
+      position: 'absolute',
+      top: 0,
+      right: 0,
+  },
+  bottomRight: {
+      position: 'absolute',
+      right: 0,
+      bottom: 0,
+  },
+
 });
