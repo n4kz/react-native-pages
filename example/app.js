ab056eae1267d69fda67a396b1c1e967582eacaa
n4kz <n4kz@n4kz.com>
2019-10-06 23:09:59 +0300

    Updated example

--- example/app.js
+++ example/app.js
@@ -1,41 +1,48 @@
 import React, { Component } from 'react';
-import { AppRegistry, StatusBar, View, Image, Animated, Platform } from 'react-native';
+import { AppRegistry, StatusBar, View, Image, Animated, Platform, StyleSheet } from 'react-native';
 import { Pages } from 'react-native-pages';
-
-let imageStyle = {
-  width: null,
-  height: null,
-  resizeMode: 'cover',
-  flex: 1,
-};
-
-let viewStyle = {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
-  overflow: 'hidden',
-};
-
-let textStyle = {
-  backgroundColor: 'transparent',
-  textAlign: 'center',
-  fontSize: 52,
-};
-
-let indexStyle = {
-  fontSize: 10,
-  color: 'rgba(255, 255, 255, .63)',
-};
 
 Platform.select({
   ios: () => StatusBar.setBarStyle('light-content'),
   android: () => StatusBar.setBackgroundColor('#263238'),
 })();
 
+let styles = StyleSheet.create({
+  image: {
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+    flex: 1,
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: '#263238',
+  },
+
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+
+  text: {
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+    fontSize: 52,
+  },
+
+  index: {
+    fontSize: 10,
+    color: 'rgba(255, 255, 255, .63)',
+  },
+});
+
 /* eslint-disable react/prop-types */
 
 let Label = ({ color, backgroundColor, text, effect, index, pages, progress }) => {
-  let style = { ...textStyle, color };
+  let style = { color };
 
   switch (effect) {
     case 'skew':
@@ -95,11 +102,11 @@ let Label = ({ color, backgroundColor, text, effect, index, pages, progress }) =
   }
 
   return (
-    <View style={[viewStyle, { backgroundColor }]}>
-      <Animated.Text style={style}>
+    <View style={[styles.textContainer, { backgroundColor }]}>
+      <Animated.Text style={[styles.text, style]}>
         {text}
         {'\n'}
-        <Animated.Text style={indexStyle}>{`[${index + 1} / ${pages}]`}</Animated.Text>
+        <Animated.Text style={styles.index}>{`[${index + 1} / ${pages}]`}</Animated.Text>
       </Animated.Text>
     </View>
   );
@@ -111,7 +118,7 @@ export default function init() {
   class Example extends Component {
     render() {
       return (
-        <View style={{ flex: 1, backgroundColor: '#263238' }}>
+        <View style={styles.container}>
           <Pages>
             <Label color='#FFF59D' backgroundColor='#607D8B' text='move' effect='skew' />
             <Label color='#B2FF59' backgroundColor='#546E7A' text='fast' effect='rise' />
@@ -126,9 +133,9 @@ export default function init() {
             indicatorColor='#FF9100'
             indicatorOpacity={0.54}
           >
-            <Image source={require('./assets/b-1.png')} style={imageStyle} />
-            <Image source={require('./assets/b-2.png')} style={imageStyle} />
-            <Image source={require('./assets/b-3.png')} style={imageStyle} />
+            <Image source={require('./assets/b-1.png')} style={styles.image} />
+            <Image source={require('./assets/b-2.png')} style={styles.image} />
+            <Image source={require('./assets/b-3.png')} style={styles.image} />
           </Pages>
         </View>
       );
